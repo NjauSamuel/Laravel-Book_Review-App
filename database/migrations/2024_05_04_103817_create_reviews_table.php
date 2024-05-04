e43c8a7af16d94ffcd792215f047f946c2cad390
NjauSamuel <njaus602@gmail.com>
2024-05-04 14:03:17 +0300

    Introduced a foreign key constraint to the reviews table as well as adding the necessarry relationships to the Book and Review Models.

--- database/migrations/2024_05_04_103817_create_reviews_table.php
+++ database/migrations/2024_05_04_103817_create_reviews_table.php
@@ -14,10 +14,19 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
 
+            //The following is an introduction of a foreign key belonging to the books table:
+            $table->unsignedBigInteger('book_id');
+
             $table->text('review');
             $table->unsignedTinyInteger('rating');
-            
+
             $table->timestamps();
+
+            //The following is how you declare the foreign key to be registered:
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+
+            // Instead of writting the whole of the two comments above, the following one line of code can handle the foreign key introduction:
+            // $table->foreignId('book_id')->constrained()->cascadeOnDelete();
         });
     }
 
