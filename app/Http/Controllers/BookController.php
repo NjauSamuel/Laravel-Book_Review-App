086cfa2e7cb365c7943badf268e9d2241c4823f6
NjauSamuel <njaus602@gmail.com>
2024-05-06 11:35:56 +0300

    Learnt about cache and how to implement it.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -30,6 +30,10 @@ class BookController extends Controller
         
         $books = $books->get();
 
+        // $cacheKey = 'books:'. $filter . ':' . $title;
+
+        // $books = cache()->remember('$cacheKey', 3600, fn() => $books->get());
+
         return view('books.index', ['books' => $books]);
     }
 
